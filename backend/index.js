192ab17c7622872a5723f805b87c01149a1be5ef
Filip Kujundzic <filipkujundzic3@gmail.com>
2024-09-17 01:21:50 +0200

    connecting to DB

--- backend/index.js
+++ backend/index.js
@@ -1,46 +1,32 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require("express")
-const mongoose = require("mongoose")
-const cors = require("cors")
-const userRouter = require("./routes/users")
-const machineRouter = require("./routes/machines")
-const customerRouter = require("./routes/customers")
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userRouter = require("./routes/users");
+const machineRouter = require("./routes/machines");
+const customerRouter = require("./routes/customers");
 const { MONGODB_URI } = process.env;
-
-
-const client = new MongoClient(MONGODB_URI, {
-    serverApi: {
-      version: ServerApiVersion.v1,
-      strict: true,
-      deprecationErrors: true,
-    }
-  });
-
 
 const corsOptions = {
     origin: "http://pipemont-mobile-app.com"
+};
+
+const app = express();
+app.use(cors(corsOptions));
+app.use(express.json());
+app.use('/users', userRouter);
+app.use('/machines', machineRouter);
+app.use('/customers', customerRouter);
+
+// Connect to MongoDB using Mongoose
+async function connectDB() {
+    try {
+        await mongoose.connect(MONGODB_URI)
+        console.log("Successfully connected to MongoDB");
+    } catch (error) {
+        console.error("Error connecting to MongoDB:", error);
+    }
 }
 
-const app = express()
-app.use(cors(corsOptions))
-app.use(express.json())
-app.use('/users', userRouter)
-app.use('/machines', machineRouter)
-app.use("/customers", customerRouter)
+connectDB();
 
-async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
-      // Send a ping to confirm a successful connection
-      await client.db("pipemont").command({ ping: 1 });
-      console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } catch(e) {
-      console.log(e)
-    }
-  }
-  run().catch(console.dir);
-
-module.exports = app
-
-
+module.exports = app;
