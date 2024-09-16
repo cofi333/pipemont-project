101e34563c4326134b7335662a77f4046dca02e5
Filip Kujundzic <filipkujundzic3@gmail.com>
2024-09-17 01:17:50 +0200

    vercel test

--- backend/index.js
+++ backend/index.js
@@ -35,9 +35,8 @@ async function run() {
       // Send a ping to confirm a successful connection
       await client.db("pipemont").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
-    } finally {
-      // Ensures that the client will close when you finish/error
-      await client.close();
+    } catch(e) {
+      console.log(e)
     }
   }
   run().catch(console.dir);
